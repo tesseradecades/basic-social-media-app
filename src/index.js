443f0805d87ef75e530c5208329ac20141e1be51
tesseradecades <nwe8978@rit.edu>
2020-02-07 07:37:54 -0500

    learning react hooks

--- src/index.js
+++ src/index.js
@@ -3,27 +3,45 @@ import ReactDOM from 'react-dom';
 
 const rootNode = document.getElementById('root');
 
-
+const base_url ='https://api.github.com/users/';
 function App(){
-    const people = ["Nate",'Nolan','Frank']
-    function handleInputChange(event){
-        const inputValue = event.target.value;
-        console.log(inputValue);
-    }
-    return(
-        <ul>
-            {people.map( (person,i)=><Person key={i} person={person}/>)}
-            <input onChange={handleInputChange}/>
-        </ul>
-    );            
-}
+    const [user, setUser] = React.useState(null);
+    const [username, setUsername] = React.useState('tesseradecades');
+    const searchInput = React.useRef();
+    React.useEffect(()=>{
+        getUser();
+    },[]);
 
-function Person(props){
-    function handlePersonClick(event){
-        alert(props.person);
-        console.log(event);
+    async function getUser(){
+        const response = await fetch(`${base_url}${username}`);
+        const data = await response.json();
+        setUser(data);
     }
-    return <li onClick={handlePersonClick}>{props.person}</li>
+
+    function handleClearInput(){
+        searchInput.current.value = '';
+        searchInput.current.focus();
+    }
+
+    return (
+    <div>
+        <input 
+            type="text" 
+            placeholder="Input username" 
+            onChange={event=>setUsername(event.target.value)}
+            ref={searchInput}
+        />
+        <button onClick={getUser}>Search</button>
+        <button onClick={handleClearInput}>Clear Input</button>
+        {user ? (
+            <div>
+                <h2>{user.name}</h2>
+                <p>{user.bio}</p>
+                <img alt="avatar" src={user.avatar_url} style={{ height: 50}}/>
+            </div>
+        ) : <p>Element loading...</p>}
+    </div>
+    );            
 }
 
 ReactDOM.render(<App/>,rootNode);
