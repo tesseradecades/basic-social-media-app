12682a44c85a62153093f7f2653c10e7ebbc5842
tesseradecades <nwe8978@rit.edu>
2020-02-09 10:54:58 -0500

    skeleton exists using reducer for post creation and deletion

--- src/App.js
+++ src/App.js
@@ -4,29 +4,34 @@ import CreatePost from './components/CreatePost';
 import Header from './components/Header';
 import Login from './components/Login';
 import PostList from './components/PostList';
+import postReducer from './reducer';
+
+export const UserContext = React.createContext();
+export const PostContext = React.createContext({
+    posts: []
+});
 
 function App(){
     const [user, setUser] = React.useState('');
-
-    const [posts, setPosts] = React.useState([]);
+    const initialPostState = React.useContext(PostContext);
+    const [state, dispatch] = React.useReducer(postReducer,initialPostState);
 
     React.useEffect(()=>{
         document.title=user ? `${user}'s feed` : 'Please Login' 
     },[user]);
 
-    const handleAddPost = React.useCallback((newPost)=>{
-        setPosts([newPost,...posts]);
-    }, [posts]);
-
     if(!user){
         return <Login setUser={setUser}/>
     }
     return (
-        <>
-            <Header user={user} setUser={setUser}/>
-            <CreatePost user={user} handleAddPost={handleAddPost}/>
-            <PostList posts={posts}/>
-        </>
+        <PostContext.Provider value={{state,dispatch}}>
+            <UserContext.Provider value={user}>
+                <Header user={user} setUser={setUser}/>
+                <CreatePost user={user} />
+                <PostList posts={state.posts}/>
+            </UserContext.Provider>
+        </PostContext.Provider>
+        
     );
 }
 
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { PostContext } from '../App';
 
-function CreatePost({user, handleAddPost}){
+function CreatePost({user}){
+    const {dispatch} = React.useContext(PostContext)
     const [content, setContent] =  React.useState('');
     const [image, setImage] = React.useState(null);
 
@@ -8,8 +10,9 @@ function CreatePost({user, handleAddPost}){
 
     function handleSubmit(event){
         event.preventDefault();
-        const post = { content, image, user};
-        handleAddPost(post);
+        const post = { content, image, user, id:Date.now()};
+        //handleAddPost(post);
+        dispatch({type:"ADD_POST", payload:{post}});
         setContent('');
         imageInputRef.current.value='';
     }
